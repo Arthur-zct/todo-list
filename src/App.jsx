5f7ebc4a9aa31f16f6e694987a2351472a79031d
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-02-25 20:54:15 -0300

    valores iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react'
 
 function App() {
-
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]) //dados de inicio
   return (
-    <div>
+    <div className='app' >
       boa tarde
     </div>
   )
