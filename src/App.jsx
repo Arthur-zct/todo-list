fe9b0be8846a11ff0b068256f927fba8b6861c3b
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-02-26 22:51:08 -0300

    criando um pedaço

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,27 @@ function App() {
       isCompleted: false,
     }
   ]) //dados de inicio
+
+  //o cara do video vai fazer tudo nesse componente, eu vou separar em diferentes componentes pra ficar certo, só passar a cons todos pra eles dps e fica tudo igual 
+
   return (
     <div className='app' >
-      boa tarde
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo)=> (
+          <section>
+            <div className='content'>
+              <p>{todo.text}</p> 
+              <p className='category'> ({todo.category}) </p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
+            </div>
+          </section>
+        ))}
+
+      </div>
     </div>
   )
 }
