de834408f03037d7d642b84622160fe044f0f75c
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-03-27 00:18:28 -0300

    ordenando alfabeticamente

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
     <div className='app' >
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className='todo-list'>
         {todos
         .filter(
@@ -78,6 +78,7 @@ function App() {
         .filter(
           (todo) => todo.text.toLowerCase().includes(search.toLowerCase())
         )
+        .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)) //"a" representa o primeiro elemento e "b" o ultimo
         .map((todo)=> (
           <Lista key={todo.id} TodoC={todo} RemoveTodo={RemoveTodo} completeTodo={completeTodo}/>
         ))}
